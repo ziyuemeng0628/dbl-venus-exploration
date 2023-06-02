dcca79ebe84493e0b38aa4cd10e6dca8b2f3f1ee
c02n-un1 <113313966+c02n-un1@users.noreply.github.com>
2023-06-02 12:18:36 +0200

    Update code.ino
    rough code innit

--- code.ino
+++ code.ino
@@ -1,3 +1,5 @@
+#define grid_size 10
+
 int mode = 0;
 int turn;
 int turncount=0;
@@ -12,9 +14,18 @@ int right=0;
 int directionnow=1;
 int x = 0;
 int y = 0;
-int x_max = 10;
-int y_max = 10;
-int map[x_max][y_max]; 
+int x_max = grid_size;
+int y_max = grid_size;
+char venusmap[grid_size][grid_size] = {"s000000000",
+                                       "0000000000",
+                                       "0000000000",
+                                       "0000000000",
+                                       "0000000000",
+                                       "0000000000",
+                                       "0000000000",
+                                       "0000000000",
+                                       "0000000000",
+                                       "0000000000"}; 
 
 String receive_data() {
   String message = "";
@@ -24,43 +35,65 @@ String receive_data() {
   return message;
 }
 
-int updatePos () {
+
+
+bool check_map(char letter){
   if (directionnow == 1) {
     y++;
-    if(map[x][y] == 'P') {
+    if(venusmap[x][y] == letter) {
       y--;
-      return y;
+      return true;
     }
   } else if (directionnow == -1) {
     y--;
-    if(map[x][y] == P) {
+    if(venusmap[x][y] == letter) {
       y++;
       turn = 1;
-      return y;
+      return true;
     }
   } else if (directionnow == 2) {
     x++;
-    if(map[x][y] == P) {
+    if(venusmap[x][y] == letter) {
       x--;
       turn = 1;
-      return x;
+      return true;
     }
   } else if (directionnow == -2) {
     x--;
-    if(map[x][y] == P) {
+    if(venusmap[x][y] == letter) {
       x++;
       turn = 1; 
-      return x;
+      return true;
     }
   }
+  
+  return false;
 }
 
-char algo(int data[3]){
+char check_forward (int US, int cliff, int rock) {
+  //obstacle check
+  if(check_map('p') || US == 1 || cliff == 1 || x>9){
+
+    return 'o';
+  }
+  //rock check
+  if(check_map('r')){
+    return 'r';
+  }else{
+    return '1';
+  }
+  
+}
+
+void update_map(char letter){
+  venusmap[][] = check_forward();
+}
+
+char algo_0(int data[3]){
   // INITIALIZE THE MAP BY FILLING IT WITH 0S 
   // AS THE POSITIONS OF OBSTACLES AND ROCKS 
   // ARE STILL UNKNOWN
-  std::fill(map[0], map[0] + x_max * y_max, 0);
-  
+
   US_check = data[0];
   IR_obst_check = data[1];
   IR_rock_check = data[2];
@@ -69,38 +102,54 @@ char algo(int data[3]){
   // 0 = FIND ROCK
   // 1 = GET ROCK
   // 2 = BACKTRACK
-  if(mode==0){
+  if(check_forward(US_check,IR_obst_check,IR_rock_check) == 'o'){ // change direction
+    
+  }
+  if(check_forward(US_check,IR_obst_check,IR_rock_check) == 'r'){
+    //change mode
+  }
+  if(check_forward(US_check,IR_obst_check,IR_rock_check) == '1'){ // go for ward
+    
+  }
     // US_check = 1 -> MOUNTAIN DETECTED
     // IR_obst_check = 1 -> BLACK TAPE DETECTED
     // IR_rock_check = 1 -> PATH DETECTED
     if (US_check==0){
       if ((IR_obst_check==0) && (IR_rock_check==0)){
-        //initial direction
+        right = 0;
         Serial.print(directionnow);
-        updatePos();
+        Serial.print("\n");
+        check_forward();
+        update_position();
         return 'f';
     
       } else if (IR_rock_check==1){
         mode=1;
+        right = 0;
         Serial.print(directionnow);
+        Serial.print("\n");
         updatePos();
         return 'f'; 
         
       } else if (IR_obst_check==1){
         if (right==0){
           right=1;
-          if (directionnow!=-2){
-            directionnow+=1;
+          if (directionnow==2){
+            directionnow = -1;
           }else if (directionnow=-2){
             directionnow=1;
-            }
+          } else {
+            directionnow++;
+          }
           Serial.print(directionnow);
+          Serial.print("\n");
           return 'r';
           }
         else{
           right=0;
           directionnow=directionnow*-1;
-          Serial.print(directionnow);;
+          Serial.print(directionnow);
+          Serial.print("\n");
           return 'b';
         }
         
@@ -109,22 +158,26 @@ char algo(int data[3]){
     } else {
       if (right==0){
           right=1;
-          if (directionnow!=-2){
-            directionnow+=1;
-          }else if(directionnow==-2){
+          if (directionnow==2){
+            directionnow = -1;
+          }else if (directionnow=-2){
             directionnow=1;
+          } else {
+            directionnow++;
           }
           Serial.print(directionnow);
+          Serial.print("\n");
           return 'r';
           }
       else{
         right=0;
         directionnow=directionnow*-1;
         Serial.print(directionnow);
+        Serial.print("\n");
         return 'b';
         }
     }
-  }
+  
 }
 
 void setup() {
